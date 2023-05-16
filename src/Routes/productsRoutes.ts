fbe436df9dd946c3fc60703638822010f41bc742
kevin otieno <kelvinotix@gmail.com>
2023-05-16 11:21:02 +0300

    Admin functionalities implemented. Verifies if someone is an admin based on their token.

--- src/Routes/productsRoutes.ts
+++ src/Routes/productsRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { addProduct, deleteProduct, getAllProductsController,getSingleProduct, updateProduct } from "../Controllers/Productscontroller";
-import { verifyLogin } from "../middleware/verifyLogin";
+import { verifyAdmin, verifyLogin } from "../middleware/verifyLogin";
 
  
  export const routerproducts=Router()
 
- routerproducts.post('/addproduct',addProduct)
+ routerproducts.post('/addproduct',verifyAdmin,addProduct)
  routerproducts.get('/getproducts',getAllProductsController)
- routerproducts.get('/getproduct/:id/',verifyLogin,getSingleProduct)
- routerproducts.put('/updateproduct/:id',verifyLogin,updateProduct)
- routerproducts.delete('/deleteproduct/:id/',verifyLogin,deleteProduct)
+ routerproducts.get('/getproduct/:id/',verifyAdmin,getSingleProduct)
+ routerproducts.put('/updateproduct/:id',verifyAdmin,updateProduct)
+ routerproducts.delete('/deleteproduct/:id/',verifyAdmin,deleteProduct)
--- src/middleware/verifyLogin.ts
+++ src/middleware/verifyLogin.ts
@@ -1,7 +1,9 @@
 import jwt from 'jsonwebtoken'
 import {Request, Response,NextFunction} from 'express'
 interface Decoded {
-    email:string
+    email:string,
+    password:string,
+    isAdmin:number
 }
 interface ExtendedRequest extends Request{
     info?:Decoded
@@ -16,6 +18,7 @@ export const verifyLogin =  async (req:ExtendedRequest, res:Response,next:NextFu
  
         //token  i need to check two things: is it expired, is it valid token
         const dedodedData = jwt.verify(token, 'ttttweywastring' as string) as Decoded
+        console.log(dedodedData)
         req.info=dedodedData
  
      } catch (error:any) {
@@ -24,3 +27,27 @@ export const verifyLogin =  async (req:ExtendedRequest, res:Response,next:NextFu
      next()
 
 }
+export const verifyAdmin =  async (req:ExtendedRequest, res:Response,next:NextFunction)=>{
+    try {
+        const token = req.headers['token'] as string
+        
+        if(!token){
+          return res.status(401).json({message:'Unathorized'})
+        } 
+ 
+        //token  i need to check two things: is it expired, is it valid token
+        const dedodedData = jwt.verify(token, 'ttttweywastring' as string) as Decoded
+        req.info=dedodedData
+        if(req.info.isAdmin!==1){
+            return res.status(401).json({message:'is not an admin'})
+        }
+    
+ 
+     }
+      catch (error:any) {
+         return res.status(403).json({message:error.message})
+     }
+     next()
+    
+}
+
