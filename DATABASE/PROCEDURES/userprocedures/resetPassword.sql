14a6ab4ecfb60bf4a706502b5049124053013d9c
kevin otieno <kelvinotix@gmail.com>
2023-05-18 11:58:17 +0300

    modified the resetPassword stored procedure to only return a success state when password has been successfuly reset

--- DATABASE/PROCEDURES/userprocedures/resetPassword.sql
+++ DATABASE/PROCEDURES/userprocedures/resetPassword.sql
@@ -1,26 +1,11 @@
-CREATE OR ALTER PROCEDURE resetPassword
-    @email VARCHAR(100),
-    @newPassword VARCHAR(100)
+CREATE OR ALTER PROCEDURE resetingPassword(
+@email VARCHAR(100),
+@newPassword VARCHAR(100))
 AS
+DECLARE @resetSuccess INT = 0;
 BEGIN
-    DECLARE @resetSuccess INT = 0;
-
-    BEGIN TRY
-        -- Update the password and set the reset status
-        UPDATE USERDB 
+	UPDATE USERDB 
         SET password = @newPassword, resetSuccess = 1
         WHERE email = @email;
 
-        -- If no rows were affected by the update, the password reset failed
-        IF @@ROWCOUNT = 0
-            SET @resetSuccess = 0;
-        ELSE
-            SET @resetSuccess = 1;
-    END TRY
-    BEGIN CATCH
-        SET @resetSuccess = 0;
-    END CATCH;
-
-    -- Return the reset status
-    SELECT @resetSuccess AS resetStatus;
 END
