5f342dc75f4ae495aaa4b82e9c33dedffa1ec2dc
kevin otieno <kelvinotix@gmail.com>
2023-05-30 21:03:17 +0300

    created a categories table , its stored procedure and controller

--- src/Routes/productsRoutes.ts
+++ src/Routes/productsRoutes.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
-import { addProduct, deleteProduct, getAllProductsController,getSingleProduct, updateProduct } from "../Controllers/Productscontroller";
-import { verifyAdmin, verifyLogin } from "../middleware/verifyLogin";
+import { addProduct, deleteProduct, getAllCategoriesController, getAllProductsController,getSingleProduct, updateProduct } from "../Controllers/Productscontroller";
+import { verifyAdmin,verifyLogin } from "../middleware/verifyLogin";
+
 
  
  export const routerproducts=Router()
 
 
-//  routerproducts.post('/addproduct',verifyAdmin,addProduct)
  routerproducts.post('/addproduct',verifyLogin,addProduct)
+//  routerproducts.post('/addproduct',verifyLogi,addProduct)
 
  routerproducts.get('/getproducts',getAllProductsController)
+ routerproducts.get('/getcategories',getAllCategoriesController)
  routerproducts.get('/getproduct/:id/',verifyAdmin,getSingleProduct)
- routerproducts.put('/updateproduct/:id',verifyAdmin,updateProduct)
+ routerproducts.patch('/updateproduct/:id',verifyAdmin,updateProduct)
  routerproducts.delete('/deleteproduct/:id/',verifyAdmin,deleteProduct)
